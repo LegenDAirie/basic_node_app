d67069d2bf6db77b917585be6ff680d106d06b8e
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-05-20 09:24:21 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//testing
+// router.get('/', function(req, res) {
+//   console.log(req.query);
+// });
+
+module.exports = router;
