2a37d7755c1ddec1ce41eda6fe33478b5caa48d8
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-05-20 09:35:52 -0700

    installed mongoose

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//testing
-// router.get('/', function(req, res) {
-//   console.log(req.query);
-// });
-
 module.exports = router;
